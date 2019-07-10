c13298ebe72b12866792e28fc45acd97ad589550
Sumaiya Tabassum <sumu1993@gmail.com>
2019-07-10 15:24:11 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,31 @@ require 'yaml'
 
 RSpec.configure do |config|
 
-  def __
+  def new_hash
     # raise "replace underscore with code"
+   new_hash = {}
   end
+
+  def my_hash
+    my_hash = {"name" => "Sumaiya", "location" => "LIC"}
+  end
+
+  def pioneer
+    pioneer = {:name => 'Grace Hopper'}
+  end
+  
+  def id_generator
+  
+  end
+
+  def my_hash_creator
+  
+  end
+
+  def read_from_hash(hash, key)
+  
+  end
+
+  def update_counting_hash
 
 end
